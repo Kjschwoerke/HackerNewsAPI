1d054bb8269ec8db75488abc13347f5a8b4d5e64
kjschwoerke <kjschwoerke@yahoo.com>
2020-01-14 20:58:46 -0600

    firstpush get api data from github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      info: [],
+      isExpanded: [],
+    }
+  }
+
+  componentDidMount() {
+
+  fetch("https://api.github.com/emojis")
+    .then(res => res.json())
+    .then(json => {
+      this.setState({
+        info: json,
+      })
+    })
+    
+  }
+
+  
+
+render(){
+  const {info} = this.state
+   console.log(info)
+   if (info.length === 0){
+    return (
+      <h1>Retrieving Data</h1>
+    )
+  }
+     return (
+      <div>
+       <img src = {info.australia}></img>
+       <img src = {info.airplane}></img>
+       </div>
+       )
+     }
+
 }
 
 export default App;
+

6bf905030bca76b373a3ac36c3d657201ca26dbc
kjschwoerke <kjschwoerke@yahoo.com>
2020-01-17 22:00:24 -0600

    sort of sorting by title

--- src/App.js
+++ src/App.js
@@ -1,47 +1,92 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
 
-class App extends Component {
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      info: [],
-      isExpanded: [],
-    }
+class App extends React.Component {
+
+  state ={
+    queryText: "",
+    results: [],
+    isLoading: false,
+    
   }
 
   componentDidMount() {
-
-  fetch("https://api.github.com/emojis")
-    .then(res => res.json())
-    .then(json => {
-      this.setState({
-        info: json,
-      })
+    this.setState({
+      results: ['type your search in the box above'],
     })
     
   }
 
-  
+//Search By Title
+  onSubmitTitle = (e) => {
+    let queryText = this.state.queryText
+    let queryString = queryText.toString()
+    let url = "http://hn.algolia.com/api/v1/search?query="+ queryString +"&tags=story"
+    
+    console.log(url)
 
-render(){
-  const {info} = this.state
-   console.log(info)
-   if (info.length === 0){
-    return (
-      <h1>Retrieving Data</h1>
-    )
+    e.preventDefault();
+    //if
+    if (this.state.queryText.length > 0) {
+      this.setState({isLoading: true})
+      fetch(url)
+      .then(res => res.json())
+      //.then
+      .then(parsedJSON =>parsedJSON.hits.map(result => ({
+        resultId: result.id,
+        createdDate: result.created_at,
+        title: result.title,
+        url: result.url,
+        author: result.author
+      })))
+//.then
+//then
+      .then(posts => {
+        this.setState({
+          results: posts,
+          isLoading: false
+        })
+      })
+//then
+    }
   }
-     return (
-      <div>
-       <img src = {info.australia}></img>
-       <img src = {info.airplane}></img>
-       </div>
-       )
-     }
 
+     onChange = (e) => {
+      this.setState({
+        queryText: e.target.value
+      })
+      
+    }
+
+    onChange2 = (e) => {
+      this.setState({
+        queryText: e.target.value
+      })
+      
+    }
+    
+render(){
+  
+     return (
+      <div className = "App">
+        <form>
+        <input placeholder = 'search by title' onChange={(e) => this.onChange(e)}/>
+        <button onClick = {this.onSubmitTitle}>Click to Search by Title</button>
+        <input placeholder = 'search by author' onChange2={(e) => this.onChange2(e)}/>
+        <button onClick = {this.onSubmitAuthor}>Click to Search by Author</button>
+        
+        {this.state.results.map(result => 
+        <>
+        <h1>{result.title}</h1>
+        <h2>{result.author}</h2>
+        <a href={result.url}>{result.url}</a>
+        </>
+        )}
+        </form>
+      </div>
+    )}
 }
 
 export default App;
